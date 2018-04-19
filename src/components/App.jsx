35242c3863d72c8e92d0db4c690d4943552642c0
Jahmanz <savojah@yahoo.com>
2018-04-19 12:38:19 -0700

    add customized 404 error message for invalid routing.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,17 +4,18 @@ import WeeklySchedule from './WeeklySchedule';
 import MonthlyProduce from './MonthlyProduce';
 import { Switch, Route } from 'react-router-dom';
 import NewDayForm from './NewDayForm';
+import Error404 from './Error404';
 
 
 function App(){
   return (
     <div>
       <Header/>
-
       <Switch>
         <Route exact path='/' component={WeeklySchedule} />
         <Route exact path='/produce' component={MonthlyProduce} />
         <Route exact path='/newday' component={NewDayForm} />
+        <Route component={Error404} />
       </Switch>
       <style global jsx>{`
           body {
